5226c15531d50ad79205064f9b3df456ce194072
Defri Indra Mahardika <defrindr@gmail.com>
2025-06-26 18:08:55 +0700

    Update heading in SearchPage to reflect accurate search context

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -60,7 +60,7 @@ export function SearchPage() {
 
   return (
     <div className="p-6 max-w-7xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4">Cari Potongan Ayah</h1>
+      <h1 className="text-3xl font-bold mb-4">Cari Makna Ayat</h1>
       <input
         type="text"
         placeholder="Cari surat..."
